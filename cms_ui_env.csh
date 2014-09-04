0358806b5c3ef41a6f96333fca1b78efcf6372a8
Stefano Belforte <stefano.belforte@cern.ch>
2014-09-04 12:16:44 +0200

    align with what's on AFS

--- cms_ui_env.csh
+++ cms_ui_env.csh
@@ -11,3 +11,14 @@ if ($SLCversion == "5") then
  source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env_3_2.11-1.csh
 endif
 
+
+
+if ($SLCversion == "6") then
+  setenv GRID_ENV_LOCATION /afs/cern.ch/cms/LCG/LCG-2/UI/DUMMY
+  echo "SLC6 detected"
+  echo " full gLite UI is not supported yet on lxplus6"
+  echo " a dummy GRID_ENV_LOCATON env.var. is defined by this script for use in Crab2"
+  echo " Note that voms*, myproxy* and lcg-* commands are in /usr/bin and"
+  echo " will work w/o any setup, if you need more, stick to lxplus5 or"
+  echo " contact CERN help desk"
+endif
--- cms_ui_env.sh
+++ cms_ui_env.sh
@@ -9,7 +9,18 @@ fi
 
 if [ $SLCversion = 5 ]
 then
-  #source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env_3_2.sh
   source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env_3_2.11-1.sh
+fi
+
+
+if [ $SLCversion = 6 ]
+then
+  export GRID_ENV_LOCATION=/afs/cern.ch/cms/LCG/LCG-2/UI/DUMMY
+  echo "SLC6 detected"
+  echo " this script defined a dummy \$GRID_ENV_LOCATION so that"
+  echo " CRAB2 with scheduler=remoteglidein will work on lxplus6, where"
+  echo " needed commands, i.e. voms*, myproxy* and lcg-*, are in /usr/bin."
+  echo "If you need full gLite UI functionalities , stick to lxplus5 or"
+  echo " contact CERN help desk"
 fi
 
